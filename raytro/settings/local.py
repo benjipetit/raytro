d1b98ff4bc2cb894689bb6b33ff7df4a037c610b
Benjamin Petit <42305218+benjipetit@users.noreply.github.com>
2018-10-20 01:44:43 +0100

    #2: provide support for Heroku deployment (#8)
    * #2: ☁ - separate settings for production and local development. Hide env vars
    
    * #2: added Procfile for Heroku
    
    * #2: added dependency psycopg2 for Postgresql
    
    * changed example PRODUCTION_HOST env variable

---
+++ raytro/settings/local.py
@@ -0,0 +1,22 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+    }
+}
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_HSTS_SECONDS             = None
+SECURE_FRAME_DENY               = False
---
+++ raytro/settings/production.py
@@ -0,0 +1,26 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [ os.environ['PRODUCTION_HOST'] ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
